40a49cf018cba9af5158da82b601e23c54b58884
rlaskarb <rlaskarb20@gmail.com>
2025-02-13 10:03:13 +0900

    Revert "2/13"
    This reverts commit 60794a0623d6985c29521d2344d3ed521c29acbd.

--- src/standardinformation/workplace/WorkplaceForm.js
+++ src/standardinformation/workplace/WorkplaceForm.js
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import "../styles/workplace-form.css";
 
 
 const WorkplaceForm = () => {
@@ -24,43 +23,20 @@ const WorkplaceForm = () => {
 
   return (
     <form className="workplace-form" onSubmit={handleSubmit}>
-      
-      <input className="workplace-input" name="name" placeholder="작업장 이름" onChange={handleChange} />
-      
-      <input className="workplace-input" name="code" placeholder="작업장 코드" onChange={handleChange} />
-      
-      <input className="workplace-input" name="location" placeholder="작업장 위치" onChange={handleChange} />
-      
-      <select className="workplace-select" name="type" onChange={handleChange}>
-        <option value="1">재료분해</option>
-        <option value="2">당화</option>
-        <option value="3">여과</option>
-        <option value="4">끓임</option>
-        <option value="5">냉각</option>
-        <option value="6">발효</option>
-        <option value="7">숙성</option>
-        <option value="8">숙성후 여과</option>
-        <option value="9">탄산조정</option>
-        <option value="10">패키징 및 출하하</option>
+      <input name="name" placeholder="작업장 이름" onChange={handleChange} />
+      <input name="code" placeholder="작업장 코드" onChange={handleChange} />
+      <input name="location" placeholder="작업장 위치" onChange={handleChange} />
+      <select name="type" onChange={handleChange}>
+        <option value="냉각">냉각</option>
+        <option value="가열">가열</option>
       </select>
-      
-      <select className="workplace-select" name="status" onChange={handleChange}>
-        <option value="a">가동</option>
-        <option value="b">정지</option>
-        <option value="c">고장</option>
-        <option value="d">수리</option>
+      <select name="status" onChange={handleChange}>
+        <option value="가동중">가동중</option>
+        <option value="정지됨">정지됨</option>
       </select>
-      
-      <input className="workplace-input" name="manager" placeholder="작업 담당자" onChange={handleChange} />
-      
-      <input name="capacity" placeholder="작업장 용량/ 생산 가능량" onChange={handleChange} />
-      
-      <select name="volume" onChange={handleChange}>
-        <option value="L">L / day </option>
-        <option value="kg">kg / day </option>
-      </select>
-      
-      <button className="workplace-filter-btn" type="submit">등록하기</button>
+      <input name="manager" placeholder="작업 담당자" onChange={handleChange} />
+      <input name="capacity" placeholder="작업장 용량" onChange={handleChange} />
+      <button type="submit">등록하기</button>
     </form>
   );
 };
--- src/standardinformation/workplace/WorkplacePage.js
+++ src/standardinformation/workplace/WorkplacePage.js
@@ -1,18 +1,14 @@
 import WorkplaceForm from "./WorkplaceForm";
-import WorkplaceTable  from "./WorkplaceTable";
-import WorkplaceFilter from "./WorkplaceFilter";
-import "../styles/workplace-form.css";
-import "../styler/workplace-table.css";
-import "../styler/workplace-filter.css";
+import WorkplaceList from "./WorkplaceList";
+
 
 
 const WorkplacePage = () => {
   return (
-    <div className="workplace-page">
-      <h2 className="workplace-title">작업장 정보 등록</h2>
-      <WorkplaceFilter/>
-      <WorkplaceForm/>
-      <WorkplaceTable/>
+    <div className="workplace-container">
+      <h2>작업장 정보 등록</h2>
+      <WorkplaceForm />
+      <WorkplaceList />
     </div>
   );
 };
