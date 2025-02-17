eb5bfea9cfa2751a4723f0ac4ac62ed9d4b9e3af
rlaskarb <rlaskarb20@gmail.com>
2025-02-17 21:45:14 +0900

    Revert "2/17  등록  및 수정 css 완료"
    This reverts commit 3611bbffd8858b858c05154a2ccebfce69d55558.

--- src/components/common/Modal.js
+++ src/components/common/Modal.js
@@ -1,22 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../styles/common/modal.css"; 
 
-const ConfirmModal = ({ isOpen, onClose, onConfirm, message }) => {
+const Modal = ({ isOpen, onClose, onConfirm, message, isFinal }) => {
   if (!isOpen) return null; // 모달이 열려있지 않으면 렌더링하지 않음
 
   return (
-    <div className="modal"  >
+    <div className="modal">
       <div className="modal-content">
         <p>{message}</p>
         <button onClick={onConfirm} className="confirm-btn">
           확인
         </button>
-        <button onClick={onClose} className="mcancel-btn">
-          취소
-        </button>
+        {!isFinal && (
+          <button onClick={onClose} className="cancel-btn">
+            취소
+          </button>
+        )}
       </div>
     </div>
   );
 };
 
-export default ConfirmModal;
+export default Modal;
